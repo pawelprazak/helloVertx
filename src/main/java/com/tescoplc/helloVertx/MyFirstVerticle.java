895bf14a6aaa169e661314508d1b8762b8974bd0
Paweł Prażak <pawelprazak@users.noreply.github.com>
2016-09-06 15:40:24 +0200

    Update MyFirstVerticle.java

--- src/main/java/com/tescoplc/helloVertx/MyFirstVerticle.java
+++ src/main/java/com/tescoplc/helloVertx/MyFirstVerticle.java
@@ -13,7 +13,7 @@ public class MyFirstVerticle extends AbstractVerticle {
                     r.response().end("<h1>Hello from my first " +
                             "Vert.x 3 application</h1>");
                 })
-                .listen(8080, result -> {
+                .listen(18080, result -> {
                     if (result.succeeded()) {
                         fut.complete();
                     } else {
